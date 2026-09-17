daf75b8267cc09d7535046b951b942111e166f06
antoinecoulon <a.antoinecoulon@gmail.com>
2026-09-17 21:45:39 +0200

    chore: add deps (ts, nuxt-hub, nuxt-ui)

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: false }
+  devtools: { enabled: false },
+
+  // typescript
+  typescript: {
+    typeCheck: true,
+    strict: true,
+  },
+
+  modules: ['@nuxt/ui', '@nuxthub/core'],
+
+  // nuxt/ui
+  css: ['~/assets/css/main.css'],
+
+  // NuxtHub
+  hub: {
+    db: "sqlite"
+  }
 })
